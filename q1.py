a899199882ede0f6c8054251753c51bf10e24ffe
Priyanshul130 <piyanshul1307@gmail.com>
2024-08-23 00:07:51 +0530

    initial commit

--- q1.py
+++ q1.py
@@ -1,4 +1,14 @@
+'''
+concider a bin file emp.dat 
+define 2 function insert_data() and display_data()
+to store and display the records in same file 
+the structure of emp.dat is EMPNo of int 
+type Ename of Str Type Esal of Float type
+'''
+
 import pickle
+
+f = open("emp.dat", "ab+")
 
 def insert_data():
     emp_no = int(input("Enter Employee Number: "))
@@ -7,24 +17,25 @@ def insert_data():
 
     emp_rec = {"EMPNo": emp_no, "Ename": ename, "Esal": esal}
 
-    with open("emp.dat", "ab") as f:
-        pickle.dump(emp_rec, f) 
-'''
-def display_data():
-    with open("emp.dat", "rb") as f:
-        while True:
-            try:s
-                emp_rec = pickle.load(f)
-                print(f"EMPNo: {emp_rec['EMPNo']}, Ename: {emp_rec['Ename']}, Esal: {emp_rec['Esal']}")
-            except EOFError:
-                break'''
+    pickle.dump(emp_rec, f) 
+    print("Record Inserted Successfully")
 
-'''while True:
+
+
+def display_data():
+    f.seek(0)  
+    while True:
+        try:
+            emp_rec = pickle.load(f)
+            print(f"EMPNo: {emp_rec['EMPNo']}, Ename: {emp_rec['Ename']}, Esal: {emp_rec['Esal']}")
+        except EOFError:
+            break
+
+while True:
     print("1. Insert Data")
     print("2. Display Data")
     print("3. Exit")
     choice = input("Enter your choice: ")
-
     if choice == "1":
         insert_data()
     elif choice == "2":
@@ -32,4 +43,5 @@ def display_data():
     elif choice == "3":
         break
     else:
-        print("Invalid choice. Please try again.")'''
+        print("Choose from 1,2,3 only")
+f.close() 
